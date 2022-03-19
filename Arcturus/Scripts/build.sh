a2dcef89c46128701b922ee5f951e435c537b250
Pi-Bouf <pierre.bouffier05@gmail.com>
2022-03-19 23:49:27 +0100

    Add more mysql

---
+++ Arcturus/Scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /app/Sources
+
+mvn package
+
+cp /app/config.ini /app/Sources/target/config.ini
+
+cd /app/Sources/target
+
+java -jar /app/Sources/target/Habbo-3.0.0-jar-with-dependencies.jar -Dfile.encoding=UTF-8 -Duser.country=FR -Duser.language=fr
