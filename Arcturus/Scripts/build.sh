d76a759052d099845c521ad3aa8fd67b0a96913c
Pierre B <pierre.bouffier@etu.univ-amu.fr>
2022-03-20 18:11:12 +0100

    Add Nitro Websocket plugin to arcturus

--- Arcturus/Scripts/build.sh
+++ Arcturus/Scripts/build.sh
@@ -5,6 +5,9 @@ supervisord -c /app/Supervisor/supervisord.conf
 cd /app/Sources
 mvn package
 cp /app/config.ini /app/Sources/target/config.ini
+mkdir /app/Sources/target/plugins
+cd /app/Sources/target/plugins
+wget https://cdn.discordapp.com/attachments/641088820598538252/873759980329766942/NitroWebsockets-3.1.jar
 
 supervisorctl start arcturus-emulator
 
