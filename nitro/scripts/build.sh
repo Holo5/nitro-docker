25ffc1120ed7001788eb3fcd87fff7edb59d4d59
Pi-Bouf <pierre.bouffier05@gmail.com>
2022-03-27 17:47:50 +0200

    Changing port, copy config,...

--- nitro/scripts/build.sh
+++ nitro/scripts/build.sh
@@ -3,6 +3,10 @@ supervisord -c /app/supervisor/supervisord.conf
 cp /app/configuration/nitro-converter/configuration.json /app/nitro-converter/src/configuration.json
 cd /app/nitro-converter; yarn install;
 
+cp /app/configuration/nitro-react/public/* /app/nitro-react/public/
+cd /app/nitro-react; yarn install;
+
 supervisorctl start swf-http-server
+supervisorctl start assets-http-server
 
 tail -f /dev/null
