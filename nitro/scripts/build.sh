06d988fa0f28c468f9af4a10b442638c6634bb25
Henri <lefebvre.hp@gmail.com>
2022-06-29 14:24:20 +0200

    Removed sass patch

--- nitro/scripts/build.sh
+++ nitro/scripts/build.sh
@@ -6,10 +6,6 @@ cd /app/nitro-converter; yarn install;
 cp /app/configuration/nitro-react/public/* /app/nitro-react/public/
 cd /app/nitro-react; yarn install;
 
-# sass patch
-yarn remove node-sass; yarn add sass; yarn install
-cp -av /app/sass-patch/LoadingView.scss /app/nitro-react/src/components/loading/
-
 supervisorctl start swf-http-server
 supervisorctl start assets-http-server
 supervisorctl start nitro-dev-server
