59e3706b0e3c75bf3ed12c4e7e098e2ba0bda99f
Henri <39599244+TheRevenge@users.noreply.github.com>
2022-07-24 17:53:26 +0200

    Removed useless line

--- emulator/scripts/build.sh
+++ emulator/scripts/build.sh
@@ -8,7 +8,6 @@ cp /app/config.ini /app/arcturus/target/config.ini
 mkdir /app/arcturus/target/plugins
 cd /app/arcturus/target/plugins
 wget https://cdn.discordapp.com/attachments/641088820598538252/873759980329766942/NitroWebsockets-3.1.jar
-#wget https://git.krews.org/Raizer/cosmic-assets/-/raw/master/Plugin/Webkit.jar
 
 supervisorctl start arcturus-emulator
 
