e34a0e68e7b162da4ef218c3091298f85d969f85
P-Bouffier <pbouffier@turbulent.ca>
2023-11-06 09:32:03 +0100

    Add workspace configuration

--- nitro/scripts/build.sh
+++ nitro/scripts/build.sh
@@ -1,10 +1,9 @@
 supervisord -c /app/supervisor/supervisord.conf
 
 cp /app/configuration/nitro-converter/configuration.json /app/nitro-converter/src/configuration.json
-cd /app/nitro-converter; yarn install;
-
 cp /app/configuration/nitro-react/public/* /app/nitro-react/public/
-cd /app/nitro-react; yarn install;
+
+cd /app/; yarn install;
 
 supervisorctl start swf-http-server
 supervisorctl start assets-http-server
