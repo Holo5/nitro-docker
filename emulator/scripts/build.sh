3a6af707d93e4919a90b2d816cd487e4a1e0cc1b
P-Bouffier <pbouffier@turbulent.ca>
2024-03-01 09:50:50 -0500

    Fix NitroWebSocket plugin link

--- emulator/scripts/build.sh
+++ emulator/scripts/build.sh
@@ -7,7 +7,7 @@ mvn package
 cp /app/config.ini /app/arcturus/target/config.ini
 mkdir /app/arcturus/target/plugins
 cd /app/arcturus/target/plugins
-wget https://cdn.discordapp.com/attachments/641088820598538252/873759980329766942/NitroWebsockets-3.1.jar
+wget https://git.krews.org/morningstar/nitrowebsockets-for-ms/-/raw/aff34551b54527199401b343a35f16076d1befd5/target/NitroWebsockets-3.1.jar
 
 supervisorctl start arcturus-emulator
 
