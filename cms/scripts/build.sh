0953e704851eeefca458bcba5c38500b45304e33
P-Bouffier <pbouffier@turbulent.ca>
2024-03-24 14:59:57 -0400

    Add the whole orion configuration

--- cms/scripts/build.sh
+++ cms/scripts/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-cd /app
 
-if [ ! -f /usr/local/bin/composer ]; then
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-    php composer-setup.php
-    php -r "unlink('composer-setup.php');"
-    mv composer.phar /usr/local/bin/composer
-fi
+cd /usr/src/orion-cms
 
-cd /app/orion-cms
+cp /usr/src/configuration/orion-cms/.env /usr/src/orion-cms/.env
+
 composer install
+yarn install && yarn build
+php artisan key:generate
+php artisan migrate --seed
+chown -R www-data:www-data /var/www/cms/
+cd /var/www/cms
+chmod -R 775 storage
+chmod -R 775 bootstrap/cache
+
+php -S 0.0.0.0:2080 -t public
